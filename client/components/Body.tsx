65ae3aed7b9dc53bcc48d46b88969ccfc68cea01
nattapatee <nattapat.eka@gmail.com>
2018-08-15 17:15:23 +0700

    fixed #19

--- client/components/Body.tsx
+++ client/components/Body.tsx
@@ -1,5 +1,5 @@
-import React, { CSSProperties } from "react"
-import { Segment, Menu, Table, Icon, Header, Message } from "semantic-ui-react"
+import React from "react"
+import { Segment, Table, Icon, Header, Dropdown, Input } from "semantic-ui-react"
 import styled from "styled-components"
 import DatePicker from "react-datepicker"
 import moment, { Moment } from "moment"
@@ -9,7 +9,6 @@ import "/css/Body.css"
 import { getApiUrl } from "../share/Configuration"
 import { LoggerApi, Log } from "../share/LoggerApi"
 import { Logs } from "./Log"
-import { start } from "repl";
 
 const BodyDiv = styled.div`
   flex-direction: column;
@@ -25,6 +24,10 @@ type State = {
     startDay: Moment
     logNow: Log[]
     noLog: boolean
+    allIp: any[]
+    allApp: any[]
+    selectIp: string
+    selectApp: string
 }
 
 export class Body extends React.Component<any, State> {
@@ -36,7 +39,11 @@ export class Body extends React.Component<any, State> {
             endDay: moment().endOf("day"),
             startDay: moment().startOf("day"),
             logNow: [],
-            noLog: true
+            noLog: true,
+            allIp: [],
+            allApp: [],
+            selectIp: "",
+            selectApp: ""
         }
     }
 
@@ -49,24 +56,112 @@ export class Body extends React.Component<any, State> {
         } else {
             this.setState({ startDay: date })
         }
-        this.initSearchByDate(date.toDate(), this.state.endDay.toDate())
+        if (this.state.selectApp === "" && this.state.selectIp === "") {
+            this.initSearchByDate(date.toDate(), this.state.endDay.toDate())
+        } else if (this.state.selectApp !== "" && this.state.selectIp === "") {
+            this.initSearchByApp(date.toDate(), this.state.endDay.toDate(), this.state.selectApp)
+        } else if (this.state.selectApp === "" && this.state.selectIp !== "") {
+            this.initSearchByIp(date.toDate(), this.state.endDay.toDate(), this.state.selectIp)
+        } else {
+            this.initSearchByAll(date.toDate(), this.state.endDay.toDate(), this.state.selectApp, this.state.selectIp)
+        }
     }
 
     public handleEndDateChange = (date) => {
         this.setState({ endDay: date });
-        this.initSearchByDate(this.state.startDay.toDate(), date.toDate())
+        if (this.state.selectApp === "" && this.state.selectIp === "") {
+            this.initSearchByDate(this.state.startDay.toDate(), date.toDate())
+        } else if (this.state.selectApp !== "" && this.state.selectIp === "") {
+            this.initSearchByApp(this.state.startDay.toDate(), date.toDate(), this.state.selectApp)
+        } else if (this.state.selectApp === "" && this.state.selectIp !== "") {
+            this.initSearchByIp(this.state.startDay.toDate(), date.toDate(), this.state.selectIp)
+        } else {
+            this.initSearchByAll(this.state.startDay.toDate(), date.toDate(), this.state.selectApp, this.state.selectIp)
+        }
+    }
+    private setIP = (_, { value }) => {
+        this.setState({ selectIp: value })
+        if (value === "All" && this.state.selectApp === "") {
+            this.setState({ selectIp: "" })
+            this.initSearchByDate(this.state.startDay.toDate(), this.state.endDay.toDate())
+        } else if (value === "All" && this.state.selectApp !== "") {
+            this.setState({ selectIp: "" })
+            this.initSearchByApp(this.state.startDay.toDate(), this.state.endDay.toDate(), this.state.selectApp)
+        } else {
+            if (this.state.selectApp === "") {
+                this.initSearchByIp(this.state.startDay.toDate(), this.state.endDay.toDate(), value)
+            } else {
+                this.initSearchByAll(this.state.startDay.toDate(), this.state.endDay.toDate()
+                    , this.state.selectApp, value)
+            }
+        }
     }
 
+    public setApp = (_, { value }) => {
+        this.setState({ selectApp: value })
+        if (value === "All" && this.state.selectIp === "") {
+            this.setState({ selectApp: "" })
+            this.initSearchByDate(this.state.startDay.toDate(), this.state.endDay.toDate())
+        } else if (value === "All" && this.state.selectIp !== "") {
+            this.setState({ selectApp: "" })
+            this.initSearchByIp(this.state.startDay.toDate(), this.state.endDay.toDate(), this.state.selectIp)
+        } else {
+            if (this.state.selectIp === "") {
+                this.initSearchByApp(this.state.startDay.toDate(), this.state.endDay.toDate(), value)
+            } else {
+                this.initSearchByAll(this.state.startDay.toDate(), this.state.endDay.toDate()
+                    , value, this.state.selectIp)
+            }
+        }
+    }
     public componentDidMount() {
+        this.initGetApp()
+        this.initGetIp()
         let starts = this.state.startDay
         let end = this.state.endDay
         this.initSearchByDate(starts.toDate(), end.toDate())
         // this.state.logNow.map((log) => console.log("this" + log))
     }
     public initSearchByDate = (StartDate: Date, EndDate: Date) => {
+        this.setState({ logNow: [] })
         this.LoggerApi.SearchByDate(StartDate, EndDate).then(response => {
             let LogDate = response.data
             this.setState({ logNow: LogDate })
+        })
+    }
+    public initSearchByIp = (StartDate: Date, EndDate: Date, Ip: string) => {
+        this.setState({ logNow: [] })
+        this.LoggerApi.SearchByIp(StartDate, EndDate, Ip).then(response => {
+            let LogDate = response.data
+            this.setState({ logNow: LogDate })
+        })
+    }
+    public initSearchByApp = (StartDate: Date, EndDate: Date, App: string) => {
+        this.setState({ logNow: [] })
+        this.LoggerApi.SearchByApp(StartDate, EndDate, App).then(response => {
+            let LogDate = response.data
+            this.setState({ logNow: LogDate })
+        })
+    }
+    public initSearchByAll = (StartDate: Date, EndDate: Date, App: string, Ip: string) => {
+        this.setState({ logNow: [] })
+        this.LoggerApi.SearchByAll(StartDate, EndDate, App, Ip).then(response => {
+            let LogDate = response.data
+            this.setState({ logNow: LogDate })
+        })
+    }
+    public initGetIp = () => {
+        this.LoggerApi.getIp().then(response => {
+            let options = response.data.map(x => ({ value: x, text: x }))
+            options.unshift({ value: "All", text: "All IP" });
+            this.setState({ allIp: options })
+        })
+    }
+    public initGetApp = () => {
+        this.LoggerApi.getApp().then(response => {
+            let options = response.data.map(x => ({ value: x, text: x }))
+            options.unshift({ value: "All", text: "All Application" });
+            this.setState({ allApp: options })
         })
     }
 
@@ -75,62 +170,73 @@ export class Body extends React.Component<any, State> {
 
         return (
             <BodyDiv>
-                <Segment inverted color="blue">
-                    <Header as="h2" icon="eye" content="Logger" />
-                </Segment>
-                <Segment attached textAlign="right" inverted color="blue" tertiary><Icon size="large" name="calendar alternate outline" />
-                    <div className="ui input datepicker">
-                        <DatePicker
-                            withPortal
-                            dateFormat="DD/MM/YY HH:mm"
-                            selected={this.state.startDay}
-                            onChange={this.handleStartDateChange}
-                            isClearable={false}
-                            placeholderText="Select Date"
-                            className="inputdate"
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeIntervals={15}
-                            timeCaption="time"
-                        />
-                        <Icon size="large" name="chevron right" />
-                        <DatePicker
-                            withPortal
-                            dateFormat="DD/MM/YY HH:mm"
-                            selected={this.state.endDay}
-                            onChange={this.handleEndDateChange}
-                            isClearable={false}
-                            placeholderText="Select Date"
-                            className="inputdate"
-                            showTimeSelect
-                            timeFormat="HH:mm"
-                            timeIntervals={15}
-                            timeCaption="time"
-                            minDate={this.state.startDay}
-                        />
-                    </div>
-                </Segment>
-                <Segment attached>
-                    <div className="logbox">
-                        {this.state.logNow.length === 0 ?
-                            <Header as="h1" icon>
-                                <br />
-                                <Icon size="huge" name="frown outline" />
-                                <br />ไม่มีบันทึก Log ในช่วงเวลา
+                <Segment.Group>
+                    <Segment textAlign="right" inverted color="blue" >
+                        <Header as="h2" floated="left">
+                            <Icon name="eye" />
+                            <Header.Content>Central Logger</Header.Content>
+                        </Header>
+                        <Icon size="large" name="box" />
+                        Application : <Dropdown className="dropdown" placeholder="All Application" closeOnChange selection options={this.state.allApp} onChange={this.setApp} value={this.state.selectApp} />
+                        &nbsp;
+                        <Icon size="large" name="address book outline" />
+                        IP : <Dropdown placeholder="All IP" closeOnChange selection options={this.state.allIp} onChange={this.setIP} value={this.state.selectIp} />
+                    </Segment>
+                    <Segment textAlign="right" inverted color="blue" >
+                        <Icon size="large" name="calendar alternate outline" />
+                        <div className="ui input datepicker">
+                            <DatePicker
+                                withPortal
+                                dateFormat="DD/MM/YY HH:mm"
+                                selected={this.state.startDay}
+                                onChange={this.handleStartDateChange}
+                                isClearable={false}
+                                placeholderText="Select Date"
+                                className="inputdate"
+                                showTimeSelect
+                                timeFormat="HH:mm"
+                                timeIntervals={15}
+                                timeCaption="time"
+                            />
+                            <Icon size="big" name="caret right" inverted />
+                            <DatePicker
+                                withPortal
+                                dateFormat="DD/MM/YY HH:mm"
+                                selected={this.state.endDay}
+                                onChange={this.handleEndDateChange}
+                                isClearable={false}
+                                placeholderText="Select Date"
+                                className="inputdate"
+                                showTimeSelect
+                                timeFormat="HH:mm"
+                                timeIntervals={15}
+                                timeCaption="time"
+                                minDate={this.state.startDay}
+                            />
+                        </div>
+                    </Segment>
+                    <Segment>
+                        <div className="logbox">
+                            {this.state.logNow.length === 0 ?
+                                <Header as="h1" icon>
+                                    <br />
+                                    <Icon size="huge" name="frown outline" />
+                                    <br />ไม่มีบันทึก Log ในช่วงเวลา
                           <Header.Subheader><br />{allday}</Header.Subheader>
-                            </Header>
-                            :
-                            <Table singleLine>
+                                </Header>
+                                :
+                                <Table singleLine>
 
-                                <Table.Body>
-                                    {this.state.logNow.map(x => <Logs logsNow={x} />)}
-                                </Table.Body>
-                            </Table>
+                                    <Table.Body>
+                                        {this.state.logNow.map(x => <Logs logsNow={x} />)}
+                                    </Table.Body>
+                                </Table>
 
-                        }
-                    </div>
-                </Segment>
-            </BodyDiv>
+                            }
+                        </div>
+                    </Segment>
+                </Segment.Group>
+            </BodyDiv >
         )
     }
 }
