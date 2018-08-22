91c852e5593b6d2da516dc0f71dadb82c8808b11
SURASAKK <surasaksermsakul@gmail.com>
2018-08-22 16:09:54 +0700

    Create Table Fixed #51

--- src/CentralLogger/Model/Centralogger.cs
+++ src/CentralLogger/Model/Centralogger.cs
@@ -16,12 +16,21 @@ namespace CentralLogger {
         public string Ip { set; get; }
         public string Category { set; get; }
     }
+
+    public class User {
+        [Key]
+        public int Id_User { set; get; }
+        public string Users { set; get; }
+        public string Password { set; get; }
+    }
+
     public enum LogLevel {
         Trace, Debug, Information, Warning, Error, Critical
     }
 
     public class CentralLoggerContext : DbContext {
         public DbSet<LogInfo> LogInfos { get; set; }
+        public DbSet<User> Users { get; set; }
         public CentralLoggerContext(DbContextOptions<CentralLoggerContext> options) : base(options) { }
     }
 }
