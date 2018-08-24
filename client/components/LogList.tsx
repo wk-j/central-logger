8cc51152950701d57d2a85cb6b369333b20c8616
nattapatee <nattapat.eka@gmail.com>
2018-08-24 17:18:23 +0700

    add infinite scroll

--- client/components/LogList.tsx
+++ client/components/LogList.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { Segment, Table, Icon, Header, Dropdown } from "semantic-ui-react"
+import { Segment, Table, Icon, Header, Dropdown, Loader } from "semantic-ui-react"
 import { Logs } from "./Log"
 import DatePicker from "react-datepicker"
 import { Moment } from "moment"
 import { Log } from "../share/LoggerApi"
 import "/css/Body.css"
-import styled from "styled-components"
+import InfiniteScroll from "react-infinite-scroll-component";
 
 type Props = {
     endDay: Moment
@@ -17,17 +17,48 @@ type Props = {
     onEndChange: (Moment) => void
     allApp: any[]
     allIp: any[]
-    // tslint:disable-next-line:variable-name
     onIpChange: (options) => void
     onAppChange: (options) => void
     selectApp: string
     selectIp: string
+    allData: Log[]
+}
+type State = {
+    items: Log[]
+    hasMore: boolean
 }
 
-export class LogList extends React.Component<Props> {
+export class LogList extends React.Component<Props, State> {
+    private limit: number;
+
     constructor(props) {
         super(props)
+        this.state = {
+            items: this.props.logNow,
+            hasMore: true,
+        }
+        this.limit = 0;
     }
+    public fetchMoreData = () => {
+        if (this.state.items.length >= this.props.allData.length) {
+            this.setState({ hasMore: false });
+            return;
+        }
+        // a fake async api call like which sends
+        // 20 more records in .5 secs
+        let limit;
+        let start;
+        if (limit <= this.props.allData.length && limit !== 0) {
+            limit = this.limit
+            start = this.limit + 100;
+            this.limit = start;
+        }
+        setTimeout(() => {
+            this.setState({
+                items: this.state.items.concat(this.props.allData.splice(start, 100))
+            });
+        }, 500);
+    };
     public setStart = (data) => {
         this.props.onStartChange(data)
     }
@@ -39,6 +70,15 @@ export class LogList extends React.Component<Props> {
     }
     public setApp = (_, { value }) => {
         this.props.onAppChange(value)
+    }
+    public componentDidUpdate(prevProps, prevState) {
+        if (prevProps.logNow !== this.props.logNow) {
+            this.setState({ items: [] })
+            let items = this.state.items
+            // items = [...items, ...this.props.logNow]
+            items = this.props.logNow
+            this.setState({ items });
+        }
     }
     public render() {
         return (
@@ -87,21 +127,39 @@ export class LogList extends React.Component<Props> {
                         />
                     </div>
                 </Segment>
-                <Segment>
-                    <div className="loglist">
+                <Segment textAlign="right">
+                    <div className="loglist" style={{ width: "100%" }}>
                         {this.props.logNow.length === 0 && !this.props.loading ?
                             <Header as="h1" icon>
                                 <br />
                                 <Icon size="huge" name="frown outline" />
                                 <br />ไม่มีบันทึก Log ในช่วงเวลา
-                  <Header.Subheader><br />{this.props.all}</Header.Subheader>
+                             <Header.Subheader><br />{this.props.all}</Header.Subheader>
                             </Header>
                             :
-                            <Table singleLine>
-                                <Table.Body>
-                                    {this.props.logNow.map((x, key) => <Logs logsNow={x} key={key} />)}
-                                </Table.Body>
-                            </Table>
+                            <InfiniteScroll
+                                dataLength={this.state.items.length}
+                                next={this.fetchMoreData}
+                                hasMore={this.state.hasMore}
+                                loader={<Loader active inline="centered" />}
+                                height={400}
+                                endMessage={
+                                    <p style={{ textAlign: "center" }}>
+                                        <b>You have seen it all</b>
+                                    </p>
+                                }
+                            >
+                                <div className="table">
+                                    <Table compact >
+                                        <Table.Body>
+                                            {
+                                                this.state.items.map((x, key) => <Logs logsNow={x} key={key} />)
+                                            }
+                                        </Table.Body>
+                                    </Table>
+                                </div>
+
+                            </InfiniteScroll>
                         }
                     </div>
                 </Segment>
