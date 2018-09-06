e8002f76ba86654d22f008db1e10f4f65f49d0b1
nattapatee <nattapat.eka@gmail.com>
2018-09-06 12:21:36 +0700

    fixed #108

--- src/CentralLogger/Controllers/LoggerController.cs
+++ src/CentralLogger/Controllers/LoggerController.cs
@@ -84,43 +84,6 @@ namespace CentralLogger.Controllers
             return Enumerable.Empty<string>();
         }
         [HttpPost]
-        public async Task<IActionResult> GetDataChart([FromBody]DateTime date)
-        {
-            List<int> countInfo = new List<int>();
-            List<int> countError = new List<int>();
-            List<int> countDebug = new List<int>();
-
-            date = date.ToLocalTime();
-            var year = date.Year;
-            var month = date.Month;
-            var day = date.Day;
-            var startDate = new DateTime(year, month, day, 0, 0, 0);
-            var endDate = new DateTime(year, month, day, 23, 59, 59);
-
-            var data = await db.LogInfos.Where(x => x.DateTime >= startDate && x.DateTime <= endDate)
-                        .Select(x => new { x.DateTime, x.LogLevel }).ToListAsync();
-
-            for (int i = 0; i <= 23; i++)
-            {
-                startDate = new DateTime(year, month, day, i, 0, 0);
-                endDate = new DateTime(date.Year, date.Month, date.Day, i, 59, 0);
-                var amountInfo = data.Where(x => (x.DateTime >= startDate && x.DateTime <= endDate) && x.LogLevel == LogLevel.Information).Count();
-                var amountError = data.Where(x => (x.DateTime >= startDate && x.DateTime <= endDate) && x.LogLevel == LogLevel.Error).Count();
-                var amountDebug = data.Where(x => (x.DateTime >= startDate && x.DateTime <= endDate) && x.LogLevel == LogLevel.Debug).Count();
-                
-                countInfo.Add(amountInfo);
-                countError.Add(amountError);
-                countDebug.Add(amountDebug);
-            }
-            return Ok(new CountLogs
-            {
-                dataInfos = countInfo,
-                dataErrors = countError,
-                dataDebugs = countDebug
-            });
-
-        }
-        [HttpPost]
         public async Task<ActionResult> AddLog([FromBody]GetLogInfos x)
         {
 
@@ -176,4 +139,5 @@ namespace CentralLogger.Controllers
             await db.Database.EnsureDeletedAsync();
         }
     }
+    
 }
