01f2f832904c8009f322b4d2a426787d8303eb35
nattapatee <nattapat.eka@gmail.com>
2018-09-11 17:55:17 +0700

    add ui manage

--- client/components/Manage.tsx
+++ client/components/Manage.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Segment, Table, Icon, Header, Dropdown, Loader, Button } from "semantic-ui-react"
+import { Segment, Table, Icon, Header, Dropdown, Modal, Button, Input, Form } from "semantic-ui-react"
 import { ManageItems } from "./ManageItems"
 import "react-datepicker/dist/react-datepicker.css";
 import { Log } from "../share/LoggerApi"
@@ -14,16 +14,28 @@ type Props = {
 }
 type State = {
     manage: manage[]
+    open: boolean
+    checked: boolean
 }
 
 export class Manage extends React.Component<Props, State> {
     constructor(props) {
         super(props)
         this.state = {
-            manage: null
+            manage: null,
+            open: null,
+            checked: true
         };
     }
-
+    public onOpens = () => {
+        this.setState({ open: true })
+    }
+    private onClose = () => {
+        this.setState({ open: false })
+    }
+    private handleChange = (value) => {
+        this.setState({ checked: value })
+    }
     public render() {
         let style = {
             marginLeft: "10px",
@@ -38,7 +50,46 @@ export class Manage extends React.Component<Props, State> {
                     </Header>
                         <Icon style={style} size="large" name="box" />
                         <Dropdown className="dropdown" placeholder="All Application" closeOnChange selection options={this.props.allApp} value={this.props.selectApp} />
-                        <Button color="olive" circular icon="plus" floated="right" />
+                        <Button onClick={this.onOpens} color="olive" circular icon="plus" floated="right" />
+                        <Modal open={this.state.open} >
+                            <Modal.Header>เพิ่มรายการใหม่</Modal.Header>
+                            <Modal.Content scrolling>
+                                <Form>
+                                    <Form.Field>
+                                        <Icon style={style} size="large" name="box" />
+                                        Application :&nbsp;<br /><br />
+                                        <Dropdown className="dropdown" placeholder="All Application" closeOnChange selection options={this.props.allApp} value={this.props.selectApp} />
+                                    </Form.Field>    <Form.Field>
+                                        <Icon style={style} size="large" name="mail" />
+                                        First Email :&nbsp;<br /><br /><Input placeholder="Email1..." width={1} />
+                                    </Form.Field>    <Form.Field>
+                                        <Icon style={style} size="large" name="mail" />
+                                        Second Email :&nbsp;<br /><br /><Input placeholder="Email2..." width={1} />
+                                    </Form.Field>    <Form.Field>
+                                        <Icon style={style} size="large" name="mail" />
+                                        Third Email :&nbsp;<br /><br /><Input placeholder="Email3..." width={1} />
+                                    </Form.Field>    <Form.Field>
+                                        <Icon style={style} size="large" name="power off" />
+                                        Enable :&nbsp;<Switch
+                                            onChange={this.handleChange}
+                                            checked={this.state.checked}
+                                            className="react-switch"
+                                            id="normal-switch"
+                                        />
+                                    </Form.Field> </Form>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color="green" floated="right" icon labelPosition="left" onClick={this.onClose}>
+                                    <Icon name="check" />
+                                    Save
+                            </Button>
+                                <Button color="red" icon labelPosition="left" onClick={this.onClose}>
+                                    <Icon name="cancel" />
+                                    Cancel
+                            </Button>
+                            </Modal.Actions>
+                        </Modal>
+
                     </Segment>
                     <Segment textAlign="center" style={{ minHeight: "calc( 100vh - 230px )" }}>
                         {this.state.manage === null ?
